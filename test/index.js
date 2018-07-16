59dffd71c8a35d6ab30a95bee9a3d980e5c935e5
minamo <minamo173dev@gmail.com>
2018-07-17 01:21:33 +0900

    fix testing

--- test/index.js
+++ test/index.js
@@ -10,12 +10,16 @@ describe('Hexo Tag Link Preview', function() {
   describe('linkPreview.getTag', function() {
     it('should return object', async () => {
         const options = {
+          image: true,
+          description: true,
           descriptionLength: 140,
           className: 'link-preview'
         }
-        const linkPreviewTag = await linkPreview.getTag({url: 'https://www.google.com/'}, options);
+        const linkPreviewTag = await linkPreview.getTag('https://www.google.com/', options);
         // output html tag
         const html = cheerio.load(linkPreviewTag).html();
+
+        expect(html).includes('<img');
         expect(html).includes('<a href="https://www.google.com/" class="link-preview">');
       }
     )
