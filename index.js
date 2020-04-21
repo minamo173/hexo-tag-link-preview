a0cc8ac565821398c73a5f1d904f83ad258e5d48
juicyjusung <juicyjusung@gmail.com>
2020-04-21 15:56:55 +0900

    fix: Fix the case where the og image url does not exist

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function getTag(options) {
       let image = '';
       let descriptions = '';
 
-      if (ogp.hasOwnProperty('ogImage')) {
+      if (ogp.hasOwnProperty('ogImage') && ogp.ogImage.url) {
         image += util.htmlTag('img', { src: ogp.ogImage.url } , '');
         image = util.htmlTag('div', { class: 'og-image'}, image)
       }
