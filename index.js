bad9d08af596876cd32053ddcffc49cca69f16fd
minamo <minamo173@users.noreply.github.com>
2020-05-22 22:20:14 +0900

    move main process to other file for testing

--- index.js
+++ index.js
@@ -8,53 +8,15 @@
 **/
 
 'use strict';
-const util = require('hexo-util');
-const ogs = require('open-graph-scraper');
-const escapeHTML = require('escape-html');
-const descriptionLength = (hexo.config.linkPreview && hexo.config.linkPreview.descriptionLength)
-                            ? hexo.config.linkPreview.descriptionLength : 140;
-const className = (hexo.config.linkPreview && hexo.config.linkPreview.className)
-                    ? hexo.config.linkPreview.className : 'link-preview';
+const getTag = require('./lib/generator');
+
+hexo.config.linkPreview = Object.assign({
+  descriptionLength: typeof hexo.config.linkPreview === 'undefined' ? 140 : hexo.config.linkPreview.descriptionLengt,
+  className: typeof hexo.config.linkPreview.className === 'undefined' ? 'link-preview' : hexo.config.linkPreview.className,
+}, hexo.config.linkPreview);
 
 hexo.extend.tag.register('linkPreview', function(args) {
   return getTag({url: args[0], target: args[1], rel: args[2]}).then(tag => {
     return tag;
   });
 }, {async: true});
-
-async function getTag(options) {
-  return ogs(options)
-    .then(function (result) {
-      const ogp = result.data;
-      let image = '';
-      let descriptions = '';
-
-      if (ogp.hasOwnProperty('ogImage') && ogp.ogImage.url) {
-        image += util.htmlTag('img', { src: ogp.ogImage.url } , '');
-        image = util.htmlTag('div', { class: 'og-image'}, image)
-      }
-
-      descriptions += util.htmlTag('div', { class: 'og-title' }, escapeHTML(ogp.ogTitle));
-
-      if (ogp.hasOwnProperty('ogDescription')) {
-        const description = adjustLength(ogp.ogDescription);
-        descriptions += util.htmlTag('div', { class: 'og-description' }, escapeHTML(description));
-      }
-
-      descriptions = util.htmlTag('div', { class: 'descriptions' }, descriptions);
-
-      const tag = util.htmlTag('div', { class: 'link-area' },  image + descriptions);
-      return util.htmlTag('a', { href: options.url, class: className, target: options.target, rel: options.rel }, tag);
-    })
-    .catch(function (error) {
-      console.log('error:', error);
-      return '';
-  });
-}
-
-function adjustLength(description) {
-  if (description && description.length > descriptionLength) {
-    description = description.slice(0, descriptionLength) + '…';
-  }
-  return description;
-}
