8d5d02b47d6f93e43ededf290f35b46258a04ce9
minamo <minamo173@users.noreply.github.com>
2020-05-22 22:20:27 +0900

    add test case

---
+++ test/index.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Hexo = require('hexo');
+
+test('get link preview from www.amazon.com', async() => {
+  const hexo = new Hexo(__dirname, {silent: true });
+  hexo.init();
+  hexo.config.linkPreview = {
+    descriptionLength: 6,
+    className: 'link-preview'
+  };
+
+  const generator = require('../lib/generator').bind(hexo);
+  const result = await generator({
+    url: 'https://ogp.me/',
+    target: '_blank',
+    rel: 'nofollow'
+  });
+
+  expect(result).toBe(
+    '<a href=\"https://ogp.me/\" target=\"_blank\" rel=\"nofollow\"><div class=\"link-area\"><div class=\"og-image\"><img src=\"http://ogp.me/logo.png\"></img></div><div class=\"descriptions\"><div class=\"og-title\">Open Graph protocol</div><div class=\"og-description\">The Op…</div></div></div></a>'
+  );
+});
